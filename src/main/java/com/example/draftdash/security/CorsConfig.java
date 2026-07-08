81201680a5afcb8c616042f3be28b9866e4f55e0
MadhumathiEswaran <emadhu912@gmail.com>
2026-07-08 16:16:01 +0530

    Update CorsConfig.java

--- src/main/java/com/example/draftdash/security/CorsConfig.java
+++ src/main/java/com/example/draftdash/security/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
     "http://localhost:5000",
     "http://localhost:5500",
     "http://127.0.0.1:5500",
-            "https://madhumathieswaran.github.io/draftdash-frontend/"
+            "https://madhumathieswaran.github.io"
 ));
         config.setAllowedMethods(List.of(
           "GET",
